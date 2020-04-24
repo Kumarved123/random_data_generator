5dc030b713e179fc66aeafe2bbb6309f1348f802
Kumarved123 <vkumar@ar.iitr.ac.in>
2020-04-24 17:48:34 +0530

    new commit

--- data_gen/environment/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\ved\desktop\submission\random_data_generator\data_gen\environment\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
