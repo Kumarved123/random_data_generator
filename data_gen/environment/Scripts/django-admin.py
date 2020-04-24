bd51fda3f1fcd45b889c81437a82eb9b32cd6079
Kumarved123 <vkumar@ar.iitr.ac.in>
2020-04-24 17:43:18 +0530

    deploying

---
+++ data_gen/environment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ved\desktop\submission\random_data_generator\data_gen\environment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
